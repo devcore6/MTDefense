c346e7beeb0e8271405c07d371995f750b8ff566
devcore6 <61387985+devcore6@users.noreply.github.com>
2022-03-24 14:01:25 +0100

    Fixed CXXGUI Submodule

--- src/cxxgui/src/include/cxxgui/gl.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace cxxgui {
-    extern void glClippingFrame(int x, int y, int w, int h);
-    extern void glPushClippingFrame();
-    extern void glPopClippingFrame();
-    extern void glClearClippingFrames();
-}
